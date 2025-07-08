a780954f6bba503cd1251af117804c01738cc081
avivka <avivka8@gmail.com>
2025-07-08 15:04:52 +0300

    chore: Update main.tf for improved module organization and clarity

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-#Description: This file is the main entry point for the IaC code. It is responsible for calling the modules that will create the network infrastructure.
+# Description: This file is the main entry point for the IaC code.
 
 
 #represent a list of "spokes", each with its own configuration
@@ -29,5 +29,35 @@ module "spokes_landing_zone" {
     azurerm.destination = azurerm.sub-hub
   }
 
+}
+
+module "aks_cluster" {
+  source = "./modules/aks"
+
+  cluster_name                  = var.cluster_name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  dns_prefix                    = var.dns_prefix
+  kubernetes_version            = var.kubernetes_version
+  sku_tier                      = var.sku_tier
+  private_cluster_enabled       = var.private_cluster_enabled
+  private_dns_zone_id           = var.private_dns_zone_id
+  private_cluster_public_fqdn_enabled = var.private_cluster_public_fqdn_enabled
+
+  local_account_disabled        = var.local_account_disabled
+  azure_rbac_enabled            = var.azure_rbac_enabled
+  admin_group_object_ids        = var.admin_group_object_ids
+
+  identity_type                 = var.identity_type
+
+  network_profile               = var.network_profile
+
+  default_node_pool             = var.default_node_pool
+  additional_node_pools         = var.additional_node_pools
+
+  providers                     = {
+    azurerm.source      = azurerm.sub-nonsap-nonprod
+    azurerm.destination = azurerm.PROD
+  }
 }
 
